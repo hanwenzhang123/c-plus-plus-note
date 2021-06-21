1b09b92a2a23430acf868240bda75da816f76a5d
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-21 12:41:44 -0400

    Create 02-datatypes-expression.cpp

---
+++ basics/02-datatypes-expression.cpp
@@ -0,0 +1,73 @@
+Data
+- int
+- float
+- double
+- char
+- string
+- bool
+- vector
+- programmer defined classes
+
+Expressions
+- i/o expressions
+- arithmetic expressions
+- boolean expreesions
+
+Control Flow
+- sequential
+- branching
+  if
+  if-else
+  if-else if-else 
+  switch
+- iterative
+  while
+  for
+- function calls
+- exceptions
+
+
+Forms of Data
+constants: can not be changed
+  programmer defined: const int MAX =5;
+  c++ literals: 6, -6, 7.3, 'abc'
+variables: can be changed later on
+  init x; double y; 
+
+
+The int Data Type
+kind of data: integer numbers
+inner representation: 
+  - each int data use 4 bytes (32 bits)
+  - the numbers are represented using the 2's complement method
+C++ literals: 3, 4, -6, 3964
+Arithmetic Operators: +, -, *, /, %, =
+  
+int main() {
+  int x;
+  int y;
+  
+  x=5;
+  
+  count<<x+2;
+  y=x+2;
+  x+2;
+  
+  count<<x-2;
+  y=x*2;
+  
+  count<<x/2;
+  count<<x%2;
+  
+  x=6;
+  cout<<x=7;
+  y=(x=8);
+  y=x=9;
+  
+  return 0;
+}
+
+13 divided by 5 = 2 R 3
+13 div 5 = 2  13 / 5
+13 mod 5 = 3  13 % 5
+
