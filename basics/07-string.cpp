19ff81233409c3b8724100a31b2f50c978fa3d64
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-21 15:26:35 -0400

    Create 07-string.cpp

---
+++ basics/07-string.cpp
@@ -0,0 +1,29 @@
+The string class
+  note: string is not a C++ built-in type. To use it you need to have: #include<string>
+  
+kind of data: Strings/Text
+
+inner representation: sequence of characters
+
+C++ literals: "abc", "this is a string \n"
+  
+Arithmetic Operators: +, =
+  
+  
+#include <iostream> 
+#include<string>
+using namespace std;
+
+int main() {
+  int x;
+  double y;
+  s = "Hello";
+  
+  cout<<s<<endl;
+  
+  cout<<s + " world"<<endl;
+  
+  s = s + "world";
+  
+  return 0;
+}
