896067a94100ac197a47a53ee92a9bf7bd20029d
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-21 12:06:11 -0400

    Update and rename 01-basics.cpp to basics/01-basics.cpp

---
+++ basics/01-basics.cpp
@@ -0,0 +1,26 @@
+//hello world
+
+
+
+
+/* this program reads two integers from the user and prints their sum */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{           //need to start with the variable with the type of the data and the name of the variable
+  int num1;   //will hold the first input
+  int num2;   //will hold the second input
+  int sum;    //will hold the sum
+  
+  //cout is output is then endl is end line, break the line
+  cout <<'please enter two numbers separated by a sace:'<<endl; //in one line is fine because the compiler knows the ;
+  cin>>num1>>num2;          //cin is getting the input from the user
+
+  sum = num1 + num2;
+  
+  cout<<num1<<' + '<<num2<<' = '<<sum<<endl;
+  
+  return 0; //program ended with the exit code: 0
+}
