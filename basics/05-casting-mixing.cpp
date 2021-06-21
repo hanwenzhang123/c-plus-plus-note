3d4968441bafffad2c559a4e2b8a0d5963d35108
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-21 14:32:14 -0400

    Update 05-casting-mixing.cpp

--- basics/05-casting-mixing.cpp
+++ basics/05-casting-mixing.cpp
@@ -8,25 +8,26 @@ int main(){
   x1 = 6;
   y1 = 6.7;
   
-  y2 = (double)6;
-  x2 = (int) 6.7;
+  y2 = (double)6;   //convert to double 6.0, y2 = 6.0
+  x2 = (int) 6.7;   //convert to integer 6, x2 = 6
   
   return 0;
 }
 
 
 Expressions with Mixed Types
+Accuracy comes first, integer can be convert to double, but double can not be converted to integer
 
 int main(){
   int x;
-  int y;
+  double y;
   
-  cout<< 5/2 <<endl;
-  cout << 5.0/2.0 <<endl;
-  cout<< 5.0/2 <<endl;      //mixing
+  cout<< 5/2 <<endl;  //div - 2
+  cout << 5.0/2.0 <<endl; //division - 2.5
+  cout<< 5.0/2 <<endl;  //mixing types, compiler will try to match the data type - 2.5
   
   x = 5/2;    //2
-  y = 5/2;    //2.0
+  y = 5/2;    //2.0 (y is double)
   
   return 0;
 }
