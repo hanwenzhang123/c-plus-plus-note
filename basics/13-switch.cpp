59a526af227a61b1c98acec926aea7a11e550e68
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-22 11:59:33 -0400

    Update 13-switch.cpp

--- basics/13-switch.cpp
+++ basics/13-switch.cpp
@@ -1,18 +1,21 @@
 Switch Statements
 
 switch(numeric-expression){
-  case constant1:
+  case constant1: //if this constant1 matches the numeric expression then run code and break, otherwise, go next
   ...
   ...
     break;
-  case constant2:
+  case constant2:  //if this constant2 matches the numeric expression then run code and break, otherwise, go next
   ...
   ...
     break;
     
-  default:
+  default:    //if no constant expressions match, the default body would be executed, break will break out the switch statement
   ...
   ...
     break;
 }
+
+
+Computing Value of a Simple Expression
 
