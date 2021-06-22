3521d21e5cd0b04d75ba15f6e404015eb393cdbc
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-22 10:33:17 -0400

    Update and rename 01 to 02-absolute-value.cpp

---
+++ branching-statement/02-absolute-value.cpp
@@ -0,0 +1,23 @@
+Computing the absolute value
+
+write a program that reads from the user an integer, and prints it is absolute value
+
+example:
+please enter an interger: -7
+  |-7| = 7
+
+  
+  
+#include <iostream> 
+using namespace std;
+
+int main() {
+  char inputChar;
+  
+  cout<<"Please enter: "<<endl;
+  cin>>inputChar;
+
+  cout<<inputChar<<endl;
+  
+  return 0;
+}
