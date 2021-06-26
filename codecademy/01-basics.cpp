56dac32d279879b5d220cba033b03fdf921a9405
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-26 17:34:23 -0400

    Create 01-basics.cpp

---
+++ codecademy/01-basics.cpp
@@ -0,0 +1,20 @@
+C++ is a general-purpose coding language.
+C++ runs line by line, from top to bottom.
+C++ is a case-sensitive language. 
+std::cout is how you output to the terminal:
+
+#include <iostream> //include libraries - iostream contains code that allows for input and output, such as displaying data in the terminal window or reading input from your keyboard.
+
+int main() //the main() function
+{         //beginning of the function
+  
+  std::cout << "Codecademy\n";    //what the program does
+  
+}        //end of the function
+
+std::cout is the “character output stream”. It is pronounced “see-out”.
+<< is an operator that comes right after it.
+"Hello World!\n" is what’s being outputted here. You need double quotes around text. The \n is a special character that indicates a new line.
+; is a punctuation that tells the computer that you are at the end of a statement. It is similar to a period in a sentence.
+   
+   
