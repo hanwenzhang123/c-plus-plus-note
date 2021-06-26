f2fd547a6af21d99975e5bab4e3aaa71f782ac6b
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-26 17:43:18 -0400

    Create 03-comments.cpp

---
+++ codecademy/03-comments.cpp
@@ -0,0 +1,15 @@
+A single line comment will comment out a single line and is denoted with two forward slashes "//" preceding it:
+
+// Prints "hi!" to the terminal
+std::cout << "hi!";
+
+You can also use a single line comment after a line of code:
+
+std::cout << "hi!";  // Prints "hi!"
+
+
+A multi-line comment will comment out multiple lines and is denoted with "/*" to begin the comment, and "*/" to end the comment:
+
+/* This is all commented.
+std::cout << "hi!";
+None of this is going to run! */
