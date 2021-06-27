754e65f8c70f2f2d3c07881dd06c3f2ee3c73aaa
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-27 16:12:07 -0400

    Update 14-first-word.cpp

--- advanced/14-first-word.cpp
+++ advanced/14-first-word.cpp
@@ -2,5 +2,27 @@ First Word Introduction
 problem
 write a program that reads from the user 3 words and prints the one that comes first in an alphabetical order
 example
-
+please enter 3 words, separated by a space: dolphin cat tiger
 cat
+
+//code
+#include <iostream> 
+#include<string>
+using namespace std;
+
+int main() {
+  string word1, word2, word3;
+  
+  cout<<"please enter 3 words, separated by a space: "<<endl;
+  cin>>word1>>word2>>word3;
+  
+  if (word1 <= word2 && word1 <= word3)
+    cout<<word1<<endl;
+  else if (word2 <= word1 && word2 <= word3)
+    cout<<word2<<endl;
+  else  //word3 is the smallest
+    cout<<word3<<endl;
+    
+  return 0;
+}
+  
