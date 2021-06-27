bc418446d7ea43eef7b0dde3131aa6162fd68642
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-26 20:13:44 -0400

    Update and rename 02-.cpp to 02-function.cpp

---
+++ advanced/02-function.cpp
@@ -0,0 +1,27 @@
+Flow of a program that calls functions
+int num -> factorial -> int(num!)
+  
+int main() {
+  int n, k, nFact, kFact, n_kFact, k_comb;
+  cout<<"Please enter n and k (k<=n): "<<endl;
+  cin>>n>>k;
+  
+  nFact = factorial(n)
+  kFact = factorial(k)
+  n_kFact = factorial(n-k) 
+    
+  k_comb = nFact / (kFact*n_kFact);
+  cout<<n<<" choose "<<k<<" is "k_comb<<endl;
+  return 0;
+}
+
+int factorial(int num){
+  int factRes, i;
+  
+  factRes = 1;
+  for (i=1, i<=num; i++)
+    factRes *= i;
+  
+  return factRes;
+}
+
