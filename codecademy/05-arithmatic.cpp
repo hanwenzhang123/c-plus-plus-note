5683a82ca9b77a1c36f505da54823691051ca040
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-01 11:38:53 -0400

    Create 05-arithmatic.cpp

---
+++ codecademy/05-arithmatic.cpp
@@ -0,0 +1,35 @@
++ addition
+- subtraction
+* multiplication
+/ division
+% modulo (divides and gives the remainder)
+  
+
+int score = 0;
+// score is 0
+ 
+score = 4 + 2;
+// it is now 6
+ 
+score = 4 - 2;
+// it is now 2
+ 
+score = 4 * 2;
+// it is now 8
+ 
+score = 4 / 2;
+// and now 2
+ 
+score = 5 % 2;
+// and now 1
+
+
+Compile:
+g++ math.cpp
+
+Execute:
+./a.out
+
+The answer should be:
+122166
+    
