284193d29afdc26118de88cc58545cdbcfe0a3bf
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-01 11:41:09 -0400

    Create 06-chaining-cout.cpp

---
+++ codecademy/06-chaining-cout.cpp
@@ -0,0 +1,15 @@
+int age = 28;
+ 
+std::cout << "Hello, I am " << age << " years old\n";
+
+
+#include <iostream>
+
+int main() {
+  
+  int score = 0;
+  
+  // Output
+  std::cout << "Player score: " << score << "\n";
+   
+}
