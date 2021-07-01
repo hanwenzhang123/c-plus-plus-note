a1a3fa034d9bf38455e923e8e000b3c84c07dcf7
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-01 11:42:26 -0400

    Create 07-input-cin.cpp

---
+++ codecademy/07-input-cin.cpp
@@ -0,0 +1,19 @@
+std::cout << "Enter your password: ";
+std::cin >> password;
+
+The name cin refers to the standard input stream (pronounced “see-in”, for character input). 
+  The second operand of the >> operator (“get from”) specifies where that input goes.
+
+  
+#include <iostream>
+
+int main() {
+  
+  int tip = 0;
+  
+  std::cout << "Enter tip amount: ";
+  std::cin >> tip;
+  
+  std::cout << "You paid " << tip << " dollars.\n";
+  
+}
