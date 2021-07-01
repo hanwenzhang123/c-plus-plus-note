c7c49e77a6bd15fbbac468a762e6efda12f20e33
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-01 12:18:52 -0400

    Create 09-relational-operators.cpp

---
+++ codecademy/09-relational-operators.cpp
@@ -0,0 +1,22 @@
+To have a condition, we need relational operators:
+
+== equal to
+!= not equal to
+> greater than
+< less than
+>= greater than or equal to
+<= less than or equal to
+
+#include <iostream>
+
+int main() {
+
+  int grade = 90;
+  
+  if (grade != 60) {
+  
+    std::cout << "Pass\n";
+  
+  }
+  
+}
