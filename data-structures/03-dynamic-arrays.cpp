9ab415ea5b0cac4361c59de5de7754fc7ff452ec
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-03 22:47:28 -0400

    Create 03-dynamic-arrays.cpp

---
+++ data-structures/03-dynamic-arrays.cpp
@@ -0,0 +1,23 @@
+//create an array that can grow
+#include <iostream>
+using namespace std;
+
+void resizeArray(int*& arr, int currentSize, int newSize){
+  int *temp = new int[newSize];
+  for (int i=0; i<currentSize; i++)
+    temp[i] = arr[i];
+  delete[] arr;
+  arr = temp;
+}
+
+int main(){
+  int size = 10;
+  int * arr = new int[size];
+  for (int i = 0; i<size; i++)
+    arr [i] = i * 10;
+  int oneMore = 110;
+  int newSize = size * 2;
+  resizeArray(arr, size, newSize);
+  size = newSize
+  arr[size++] = oneMore;
+}
