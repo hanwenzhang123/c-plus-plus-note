0259ccf235081bd7e45b4da7d1aa2efa41c09295
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-07-03 23:16:55 -0400

    Update 05-recursions.cpp

--- data-structures/05-recursions.cpp
+++ data-structures/05-recursions.cpp
@@ -1,1 +1,16 @@
-
+//mathematical induction overview
+mathematical induction is a technique to prove universal statements:
+"P(n) is true for any natural number n"
+ 
+there are 2 steps un such proofs:
+1. base case: we prove that P(1) is true
+2. inductive step: we know that for any n>=2, p(n-1) -> p(n)
+(that is we assume that P(n-1) is true, and under this assumption, we know that P(n) is true)
+  
+We proved:  Conclusion:
+P(1)             P(1)
+P(1)->P(2)       P(2)
+P(2)->P(3)       P(3)
+P(3)->P(4)       P(4)
+  
+//mathematical induction example
